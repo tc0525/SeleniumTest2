9d5ddde233300b1e21c147e1223d6339bd38b663
tc0525 <dfhdfhf_19751204new>
2022-04-06 21:47:20 +0300

    done

---
+++ src/test/java/selenium/SecondTest.java
@@ -0,0 +1,49 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class SecondTest {
+    @Test
+    public void firstTest() throws InterruptedException {
+
+        System.setProperty("webdriver.chrome.driver", "D:/Automated Testing/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.manage().window().maximize();
+
+        driver.get("https://selectorshub.com/xpath-practice-page/");
+        Thread.sleep(5000);
+
+        WebElement userEmail = driver.findElement(By.id("userId"));
+        userEmail.sendKeys("test@test.com");
+
+        Thread.sleep(5000);
+
+        WebElement password = driver.findElement(By.id("pass"));
+        password.sendKeys("pass123");
+
+        Thread.sleep(5000);
+
+        WebElement company = driver.findElement(By.name("company"));
+        company.sendKeys("qa company");
+
+        Thread.sleep(5000);
+
+        WebElement submit = driver.findElement(By.xpath("//input[@type='submit']"));
+        submit.click();
+
+        Thread.sleep(5000);
+
+        driver.quit();
+
+    }
+}
