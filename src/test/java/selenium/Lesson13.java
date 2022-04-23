d2116dc55499cd5bfa7b652a9bc3ca9ece9dac85
tc0525 <102527584+tc0525@users.noreply.github.com>
2022-04-23 20:57:00 +0300

    Комментарии преподавателя к задаче 2 и задаче 3

--- src/test/java/selenium/Lesson13.java
+++ src/test/java/selenium/Lesson13.java
@@ -5,6 +5,15 @@ import java.util.Scanner;
 public class Lesson13 {
     public static void main(String[] args) {
 
+        //Задача 3 (комментарий преподавателя)
+        for (int i = 40; i <= 60; i = i + 4)
+            System.out.println(i);
+
+        //Задача 2  (комментарий преподавателя)
+        for (int i = 5; i < 10000; i = i * 5)
+            System.out.println(i);
+
+
 //Задача 1
 
         int i;
@@ -22,6 +31,7 @@ public class Lesson13 {
             power++;
             result = result * number;
         } while (result < 10000); // условие выхода из цикла
+
 
 //Задача 3
 
