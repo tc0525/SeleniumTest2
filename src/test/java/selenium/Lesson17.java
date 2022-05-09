3e9ecb3039190183d5d48b8db569696bc3f136e9
tc0525 <102527584+tc0525@users.noreply.github.com>
2022-05-09 17:31:09 +0300

    Задача 1

---
+++ src/test/java/selenium/Lesson17.java
@@ -0,0 +1,14 @@
+package selenium;
+
+public class Lesson17 {
+    public static void main(String[] args) {
+        int x = 200;
+        int y = 20;
+
+        System.out.println(x + y);
+        System.out.println(x - y);
+        System.out.println(x * y);
+        System.out.println(x / y);
+    }
+}
+
